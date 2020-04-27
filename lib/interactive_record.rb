8760dd40ad053dbb0009b70aa3ef204bd6900261
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-27 00:55:43 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -40,8 +40,7 @@ class InteractiveRecord
   end
   
   def column_name_for_insert
-    self.class.column_names.delete_if  |col|
-      col == "id"
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
     end
   end
   
